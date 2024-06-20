62c1dff2832b0aa1eb89909987dc4051a396abf4
githubhiseka <susantoabhiseka@gmail.com>
2024-06-20 23:58:56 +0700

    CANT WORK

--- js/script.js
+++ js/script.js
@@ -8,28 +8,41 @@ const reset = document.querySelector(".reset");
 const formula = document.querySelector(".formula");
 
 swap.onclick = toFahr;
-convert.onclick = convertDegree;
+convert.onclick = convertDegree(celcText);
 reset.onclick = zeroDegree;
 
 function toFahr() {
-    console.log("to fahrenthai");
+    // console.log("to fahrenthai");
     celcText.innerText = "Fahrenheit";
     fahrText.innerText = "Celcius";
+    const celcTemp = celc.value;
+    celc.value = fahr.value;
+    fahr.value = celcTemp;
     swap.onclick = toCelc;
 }
 
 function toCelc() {
-    console.log("to celcius");
+    // console.log("to celcius");
     celcText.innerText = "Celcius";
     fahrText.innerText = "Fahrenheit";
+    const celcTemp = celc.value;
+    celc.value = fahr.value;
+    fahr.value = celcTemp;
     swap.onclick = toFahr;
 }
 
-function convertDegree() {
+function convertDegree(degree) {
     const derajat = celc.value;
     console.log(derajat);
+    if (degree == "Celcius") {
+        console.log("nice");
+        fahr.value = (parseInt(celc.value) * (9/5)) + 32;
+    } else if (degree = "Fahrenheit") {
+
+    }
 }
 
-function zeroDegree(params) {
-    
+function zeroDegree() {
+    celc.value = '';
+    fahr.value = '';
 }
